2befd03a2fc128a023c6ad8a4620abe1018e18a4
Seth Nelson <seth.nelson@outlook.com>
2020-09-30 16:58:57 -0700

    added userDefinedFunction practice

--- listComprehensions.py
+++ listComprehensions.py
@@ -1,9 +1,7 @@
-# list comprehension example format
 # [<map expression> for <name> in <sequence expression> if <filter expression>]
 
-"""
-Use a list comprehension to create a new list called new_list out of the numbers list. Use the list comprehension to make sure that the new_list only contains positive numbers and make sure they are cast as integers into the new_list.
-"""
+#Use a list comprehension to create a new list called new_list out of the numbers list. Use the list comprehension to make sure that the new_list only contains positive numbers and make sure they are cast as integers into the new_list.
+
 
 numbers = [22.3, -184.4, 57.8, 99.6, -18.2, 84.2, 71.3]
 
---
+++ userDefinedFunctions.py
@@ -0,0 +1,25 @@
+# def function_name(argument_1, argument_2, etc.):
+    # function line 1
+    # function line 2
+    # etc.
+    
+    
+"""
+Modify this function to make it return the sum of the arguments a and b.
+"""
+def sum(a, b):
+    return a + b
+
+# This should print 7
+print(sum(2, 5))
+
+
+"""
+Modify this function to use the sum function above to return
+the double of the sum of a and b.
+"""
+def double_the_sum(a, b):
+    return (a + b) * 2
+
+# This should print 14
+print(double_the_sum(2, 5))
